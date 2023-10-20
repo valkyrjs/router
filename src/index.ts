b5ad9432f52c59909cf3f89c6e70bc5bfab8f0a5
kodemon <hi@kodemon.net>
2023-10-20 10:13:49 +0000

    chore(log): remove console log

--- src/index.ts
+++ src/index.ts
@@ -7,5 +7,3 @@ export type { RoutedResult } from "./Router.js";
 export { Router } from "./Router.js";
 export type { Location } from "history";
 export { createBrowserHistory, createHashHistory, createMemoryHistory } from "history";
-
-console.log("Hello Worlds");
